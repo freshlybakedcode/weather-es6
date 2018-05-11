6e232d37c0a62b33e97ce46dd3b7cb79e1632dfc
James Brenton <james@jamesbrenton.co.uk>
2018-05-11 14:08:18 +0100

    Improved hashing

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
+// webpack v4
 const path = require("path");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const WebpackMd5Hash = require("webpack-md5-hash");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: { main: "./src/js/index.js" },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash].js"
   },
-  devtool: 'source-map',
   module: {
     rules: [
       {
@@ -20,22 +21,28 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: ["css-loader", "sass-loader"]
-        })
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin(
-      {filename: 'style.[chunkhash].css', disable: false, allChunks: true}
-    ),
+    // new ExtractTextPlugin(
+    //   {filename: 'style.[hash].css', disable: false, allChunks: true }
+    // ),
+    new MiniCssExtractPlugin({
+      filename: "style.[contenthash].css"
+    }),
     new HtmlWebpackPlugin({
       inject: false,
       hash: true,
       template: "./src/index.html",
       filename: "index.html"
-    })
+    }),
+    new WebpackMd5Hash()
   ]
 };
