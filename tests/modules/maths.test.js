b56c373bfcca23094638f09634c546acaed1f7eb
James Brenton <james@jamesbrenton.co.uk>
2018-05-15 16:48:25 +0100

    More stable testing

--- tests/modules/maths.test.js
+++ tests/modules/maths.test.js
@@ -3,6 +3,9 @@ import {add} from '../../src/js/modules/maths';
 
 describe('maths.js tests', function() {
     describe('add()', function() {
+        it('should exist', function() {
+            assert.notStrictEqual(add(), undefined);
+        });
         it('should add two numbers', function() {
             assert.equal(add(4,3), 7);
         });
